116cc9f37cdc0923272a6aad3c4812eeaa542777
WEN1WEN <1935862865@qq.com>
2017-07-11 14:33:40 +0800

    Update move_zeroes.cpp

--- Array/283. Move Zeroes/move_zeroes.cpp
+++ Array/283. Move Zeroes/move_zeroes.cpp
@@ -1,6 +1,7 @@
 List List class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // i, j are just like two pointers. i points to the element behind 0. j points 0.
         for (int i = 0, j = 0; i < nums.size(); i++)
             if (nums[i] != 0)
                 swap(nums[i], nums[j++]);
