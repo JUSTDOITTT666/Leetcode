c7dffa5e602554f2eb3426e1fcf37dabca1d29d4
WEN1WEN <1935862865@qq.com>
2017-07-11 14:28:05 +0800

    Update move_zeroes.cpp

--- Array/283. Move Zeroes/move_zeroes.cpp
+++ Array/283. Move Zeroes/move_zeroes.cpp
@@ -1,1 +1,8 @@
-
+List List class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int i = 0, j = 0; i < nums.size(); i++)
+            if (nums[i] != 0)
+                swap(nums[i], nums[j++]);
+    }
+};
