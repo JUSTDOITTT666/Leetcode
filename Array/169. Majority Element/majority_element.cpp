9407695808e554b7f333e0d58618a0468ea9da03
WEN1WEN <1935862865@qq.com>
2017-07-11 16:04:56 +0800

    Update majority_element.cpp

--- Array/169. Majority Element/majority_element.cpp
+++ Array/169. Majority Element/majority_element.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        unordered_map<int, int> hash;        
+        int n = nums.size() / 2;
+        
+        for (int num : nums) 
+            if (++hash[num] > n)
+                return num; 
+    }
+};
