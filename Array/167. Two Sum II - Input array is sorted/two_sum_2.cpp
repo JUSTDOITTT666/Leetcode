8ce5a4e2b13e1a3bff427ecead69e707839c4f04
WEN1WEN <1935862865@qq.com>
2017-07-11 15:14:26 +0800

    Update two_sum_2.cpp

--- Array/167. Two Sum II - Input array is sorted/two_sum_2.cpp
+++ Array/167. Two Sum II - Input array is sorted/two_sum_2.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        // lo points to the first, and hi points to the last
+        int lo = 0, hi = numbers.size() - 1;
+        
+        while (numbers[lo] + numbers[hi] != target) {
+            if (numbers[lo] + numbers[hi] < target) 
+                lo++;
+            else
+                hi--;      
+        }
+        
+        return vector<int>({lo + 1, hi + 1});
+    }
+};
